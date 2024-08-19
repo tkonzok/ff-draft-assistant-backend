66adb76c0c64dab6827037f641a6197aa89bb059
tobiaskonzok <tobias.konzok@shinefour.de>
2024-08-19 16:55:14 +0200

    deploy api to fly.io

--- src/ranking/ranking.module.ts
+++ src/ranking/ranking.module.ts
@@ -3,10 +3,6 @@ import { RankingService } from "./ranking.service";
 import { RankingController } from "./ranking.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Player } from "./player.entity";
-import {Draft} from "../draft/draft.entity";
-import {DraftService} from "../draft/draft.service";
-import {DraftController} from "../draft/draft.controller";
-import {DraftModule} from "../draft/draft.module";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Player])],
